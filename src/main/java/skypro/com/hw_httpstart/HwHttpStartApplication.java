4341bd217022a8d53312e404ec927ada39241778
Scherbakova <nastyasch0806@gmail.com>
2023-01-22 21:16:09 +0300

    first commit

---
+++ src/main/java/skypro/com/hw_httpstart/HwHttpStartApplication.java
@@ -0,0 +1,13 @@
+package skypro.com.hw_httpstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwHttpStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwHttpStartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/skypro/com/hw_httpstart/HwHttpStartApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.com.hw_httpstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwHttpStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
